e2f428e960043a53cdfc08d0435c443e8536559a
ethan-pt <tubbeethan@gmail.com>
2023-05-27 15:21:34 -0700

    add budget info model

--- budgeting_app/models.py
+++ budgeting_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class BudgetInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    initial_balance = models.FloatField()
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    amount = models.FloatField()
+    date = models.DateField(auto_now_add=True, editable=True, blank=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['date']
