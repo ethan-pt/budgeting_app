bc42c2c9497331e2f90418b13a73201d53ed5a25
ethan-pt <tubbeethan@gmail.com>
2023-05-27 23:27:31 -0700

    add description field, change balance name

--- budgeting_app/models.py
+++ budgeting_app/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 
 class BudgetInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    initial_balance = models.FloatField()
+    balance = models.FloatField()
     title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
     category = models.CharField(max_length=100)
     amount = models.FloatField()
     date = models.DateField(auto_now_add=True, editable=True, blank=False)
