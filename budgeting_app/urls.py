19a7f7437cf01aa9d4d4da9a1da7ae83f42e5486
ethan-pt <tubbeethan@gmail.com>
2023-05-27 23:58:20 -0700

    add url patterns

--- budgeting_app/urls.py
+++ budgeting_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BudgetLogin, BudgetRegister, BudgetList
+from .views import BudgetLogin, BudgetRegister, TransactionList, TransactionDetail, TransactionCreate, TransactionDelete, TransactionUpdate
 from django.contrib.auth.views import LogoutView
 
 
@@ -8,5 +8,9 @@ urlpatterns = [
     path('login/', BudgetLogin.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', BudgetRegister.as_view(), name='register'),
-    path('', BudgetList.as_view(), name='budget')
+    path('', TransactionList.as_view(), name='budget'),
+    path('transaction/<int:pk>/', TransactionDetail.as_view(), name='transaction'),
+    path('transaction-create/', TransactionCreate.as_view(), name='transaction-create'),
+    path('transaction-update/<int:pk>/', TransactionUpdate.as_view(), name='transaction-update'),
+    path('transaction-delete/<int:pk>/', TransactionDelete.as_view(), name='transaction-delete'),
 ]
