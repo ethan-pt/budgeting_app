3669b959a27cbf478d31a17be4aebf5649c0aa85
ethan-pt <tubbeethan@gmail.com>
2023-05-30 13:18:04 -0700

    add default values to balance, amount, and date

--- budgeting_app/models.py
+++ budgeting_app/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 
 class BudgetInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    balance = models.FloatField()
+    balance = models.FloatField(default=0.00)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     category = models.CharField(max_length=100)
-    amount = models.FloatField()
-    date = models.DateField(auto_now_add=True, editable=True, blank=False)
+    amount = models.FloatField(default=0.00)
+    date = models.DateField(default=timezone.now, blank=False)
 
     def __str__(self):
         return self.title
