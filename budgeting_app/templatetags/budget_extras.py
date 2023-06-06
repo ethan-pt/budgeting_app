bd4b8f56a3d76eb781c171cd8976f863ab3393aa
ethan-pt <tubbeethan@gmail.com>
2023-06-05 20:34:59 -0700

    remove sort function, add as_currency function

--- budgeting_app/templatetags/budget_extras.py
+++ budgeting_app/templatetags/budget_extras.py
@@ -6,22 +6,15 @@ from collections import OrderedDict
 
 register = template.Library()
 
-# Sort transactions by selected heading
+# Sort transactions by selected heading, reversing the order on second click
 @register.simple_tag
-def url_replace(request, field, value, direction=''):
-    dict_ = request.GET.copy()
+def url_replace(data, value, direction=''):
+    pass
 
-    if field == 'order_by' and field in dict_.keys():
-        if dict_[field].startswith('-') and dict_[field].lstrip('-') == value:
-            dict_[field] = value
-        
-        elif dict_[field].lstrip('-') == value:
-            dict_[field] = '-' + value
-        
-        else:
-            dict_[field] = direction + value
-        
+
+@register.simple_tag
+def as_currency(amount):
+    if amount >= 0:
+        return '+${:,.2f}'.format(amount)
     else:
-        dict_[field] = direction + value
-
-    return urlencode(OrderedDict(sorted(dict_.items())))
+        return '-${:,.2f}'.format(-amount)
